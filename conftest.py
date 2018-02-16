f35c9eb7151bf6815124b903d7fd8d34d7459017
olgaduma <olga.duma17@gmail.com>
2018-02-16 11:53:47 +0200

    git commit -m "init_role update @elpiankova"

--- conftest.py
+++ conftest.py
@@ -9,16 +9,11 @@ def app_url():
 def book(app_url):
     response = requests.post(app_url + "books/", data={"title": "AnnaK", "author": "LevT"})
     return response.json()
-    #book = response.json()
-   # yield book
-    #r = requests.delete(app_url + "books/{}/".format(book["id"]))
-
-    #response = requests.post(app_url + "books/", data={"title": "Anna2", "author": "Lev2"})
-    #return response.json()
-    # yield book  # additional deleting 3 strings
-    # r = requests.delete(app_url + "books/{}/".format(book["id"]))
-    #role = response.json()
 
 @pytest.fixture()
 def role(book):
     return {"name": "Peppi Longsocks", "type": "orange-hero", "level": 1, "book": book["id"]}
+
+@pytest.fixture()
+def init_role(book):
+    return {"name": "Mumu", "type": "animal", "level": 1, "book": book["id"]}
--- test_role_update.py
+++ test_role_update.py
@@ -1,8 +1,12 @@
 import requests
 
+def test_init_role_created(app_url, init_role):
+    response = requests.post(app_url + "roles/", data=init_role)
+    assert response.status_code == 201
 
-def test_role_update(app_url, role):
+
+def test_role_update(app_url,role, init_role):
     role['name'] = 'Big bear'
-    response = requests.put(app_url + 'roles/{}/'.format (role['id']), data=role)
+    response = requests.put(app_url + 'roles/{}/'.format (init_role['id']), data=role)
     assert response.status_code == 200
     #assert response.json() == role
